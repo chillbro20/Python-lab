edebbf6cd2269bc36abc93dde2f233fbdd2c7aaa
SAURABH <97622677+chillbro20@users.noreply.github.com>
2022-04-26 20:04:08 +0530

    face_detection_img

--- computer_vision/face_detection_img.py
+++ computer_vision/face_detection_img.py
@@ -4,11 +4,13 @@ import mediapipe as mp
 mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
 
-IMAGE_FILES = ['computer_vision/image1.jpg','computer_vision/image2.jpg','computer_vision/image3.jpg']
+IMAGE_FILES = ['computer_vision/image1.jpg','computer_vision/image2.jpg']
 
 with mp_face_detection.FaceDetection( model_selection=1, min_detection_confidence=0.5) as face_detection:
     for idx, file in enumerate(IMAGE_FILES):
+        
         image = cv2.imread(file)
+        image = cv2.resize(image,(1000,1000))
         
         results = face_detection.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
         if not results.detections:
